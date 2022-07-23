4826cbe4b7a303dcc4aa23829e23a67f051b56da
Sabtha Rishi <87591206+Sabtha-Rishi@users.noreply.github.com>
2022-07-23 18:50:17 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const https = require('https');
 
 
 const app = require('./app');
@@ -6,7 +6,7 @@ const app = require('./app');
 require('dotenv').config()
 
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 
 server.listen(8000, () => {
     console.log(`Listening on port ${8000}....`)
